fdf399c82a8dd44b01a2f2925b6c19ed389b64a0
tonystark <chandravicky288@gmail.com>
2023-06-23 11:53:22 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [inp, setInp] = useState("");
+  const [sum, setSum] = useState(0);
+  useEffect(()=>{
+    setSum(()=> sum+inp);
+  },[inp]);
+
   return (
     <div>
+      <h1>Sum Calculator</h1>
         {/* Do not remove the main div */}
+        <input type="number" onInput={(e) => setInp(+(e.target.value))}></input>
+        <p>Sum: {sum}</p>
+    
     </div>
   )
 }
 
-export default App
+export default App;
